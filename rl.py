3fcf838acc7f38467378ecc052e264b33a072f54
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2019-11-05 16:11:29 +0900

    first commit

--- rl.py
+++ rl.py
@@ -4,8 +4,7 @@ def load_linear_example1():
     """
     >>> X,Y = load_linear_example1()
     >>> print(X[0])
-    [1,4]
-    :return: 
+    [1 4]
     """
     X=np.array([[1,4],[1,8],[1,13],[1,17]])
     Y=np.array([7, 10, 11, 14])

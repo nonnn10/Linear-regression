435592b52221c0b3b3d82c15386e338301161b23
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2019-11-05 16:25:20 +0900

    fit add

--- regression.py
+++ regression.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 class LinearRegression:
-    
+
     x = None
     theta = None
     y = None
 
     def fit(self, x, y):
-        pass
+        temp = np.linalg.inv(np.dot(x.T, x))
+        self.theta = np.dot(np.dot(temp, x.T), y)
 
     def predict(self, x):
         pass

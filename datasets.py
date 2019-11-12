f1e04254a6a14168b22752efb833092ad73c3e8e
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2019-11-12 15:28:31 +0900

    nonlinear add

--- datasets.py
+++ datasets.py
@@ -10,3 +10,17 @@ def load_linear_example1():
     Y=np.array([7, 10, 11, 14])
     return X,Y
 
+def load_nonlinear_example1():
+    """
+    >>> X,Y = load_nonlinear_example1()
+    >>> print(X[0])
+    [1 0.0]
+    :return: 
+    """
+    X = np.array([[1, 0.0], [1, 2.0], [1, 3.9], [1, 4.0]])
+    Y = np.array([4.0, 0.0, 3.0, 2.0])
+    return X, Y
+
+def polynomial2_features(input):
+    poly2 = input[:,1:]**2
+    return np.c_[input, poly2]
